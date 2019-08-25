ddc9e181a564a9532f8b3645979d6bc8506ebe2b
Luca Faraoni <faraoni.1667345@studenti.uniroma1.it>
2019-08-25 11:07:17 +0200

    moved js code in a separate file

---
+++ pac3D.js
@@ -0,0 +1,104 @@
+var scene, camera, renderer;
+var mouse, raycaster;
+
+var cube;
+
+function init() {
+    // Create the scene
+    scene = new THREE.Scene();
+
+    //Create the main camera
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    //Create the renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    //Set up the main camera
+    camera.position.set(0, 0, 100);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    //Create and add a source of light
+    var dirLight = new THREE.DirectionalLight();
+    dirLight.position.set(25, 23, 15);
+    scene.add(dirLight);
+
+    cube = new THREE.Mesh(
+        new THREE.BoxGeometry(10, 10, 10),
+        new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+    );
+    scene.add(cube);
+    
+    //Used to add events listenders
+    const domEvents = new THREEx.DomEvents(camera, renderer.domElement)
+
+    //Create a raycaster instances useful to object picking and other things
+    mouse = { x : 0, y : 0 };
+    raycaster = new THREE.Raycaster();
+    renderer.domElement.addEventListener('click', raycast, false);
+
+    animate();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    //controls.update();
+    renderer.render(scene, camera);
+}
+
+function raycast (e) {
+
+    //Sets the mouse position with a coordinate system where the center of the screen is the origin
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+
+    //Set the picking ray from the camera position and mouse coordinates
+    raycaster.setFromCamera(mouse, camera);    
+
+    //Compute intersections
+    var intersects = raycaster.intersectObjects(scene.children);
+     /*
+        An intersection has the following properties :
+            - object : intersected object (THREE.Mesh)
+            - distance : distance from camera to intersection (number)
+            - face : intersected face (THREE.Face3)
+            - faceIndex : intersected face index (number)
+            - point : intersection point (THREE.Vector3)
+            - uv : intersection point in the object's UV coordinates (THREE.Vector2)
+    */
+
+    for (var i = 0; i < intersects.length; i++) {
+        console.log(intersects[i]); 
+        intersects[i].object.material.color.setHex(0xffffff);
+       
+    }
+}
+
+/* var loader = new THREE.FontLoader();
+loader.load("fonts/Plastic_Fantastic_Regular.json", function (font) {
+
+    var text = new THREE.TextGeometry('Hello three.js!', {
+        font: font,
+        size: 50,
+        height: 10,
+        curveSegments: 0,
+    } );
+
+    var textMaterial = new THREE.MeshPhongMaterial({color: 0xff0000});
+    var mesh = new THREE.Mesh(text, textMaterial);
+    mesh.position.set(-400, 150, -200);
+
+    scene.add(mesh);
+
+    domEvents.addEventListener(mesh, "mouseover", event => {
+        mesh.material.color.setHex(0xffffff);
+    })
+
+    domEvents.addEventListener(mesh, "mouseout", event => {
+        mesh.material.color.setHex(0xff0000);
+    })
+} ); */
+
+window.onload = init;
