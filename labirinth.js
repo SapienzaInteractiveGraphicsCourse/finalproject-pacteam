c89f19d88e711ca5127bd6df9116cef1169bfba6
GoodLuck95 <faraoni.1667345@studenti.uniroma1.it>
2019-09-07 12:23:46 +0200

    added labirinth class

---
+++ labirinth.js
@@ -0,0 +1,117 @@
+/*
+
+A class to represents a Labirinth
+
+*/
+
+
+
+class Labirinth {
+
+    constructor() {
+        //Represents the maze's blocks
+        this.maze = new Array(42);
+        for (i = 0; i < this.maze.length; i++) {
+            this.maze[i] = new Array(41);
+        }
+
+        //Represents the balls into the this.maze
+        this.balls = [];
+    }
+
+    createMaze() {
+        for (i=0; i < this.maze.length; i++) {
+            for (j=0; j < this.maze[0].length; j++) {
+                if (i == 0 || i == this.maze.length-1) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (j == 0 || j==this.maze[0].length-1) && (i < 17 || i > 27) ) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i==16 || i== 28 || i==20 || i==24) && (j<8 || j>=32) ) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (j==8 || j==32) && ( (i>=16 && i<=20) || (i>=24 && i<=28) ) ) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if (i == 4 && ( (j>3 && j<17) || j==20 || (j > 23 && j < 37) )) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ((i >= this.maze.length-4) && (j==20)) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ((i == this.maze.length - 6 || i == this.maze.length - 5) && ((j == 20) || (j >= 4 && j <= 8) || (j >= 12 && j <= 16) || (j >= 24 && j <= 28) || (j >= 32 && j <= 36))) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ((i == this.maze.length - 10) && ((j >= 4 && j <= 8) || (j == 12) || (j >= 16 && j <= 24) || (j == 28) || (j >= 32 && j <= 36))) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ((i <= this.maze.length - 11 && i >= this.maze.length - 14) && ((j == 12) || (j == 20) ||   (j == 28))) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ((i <= this.maze.length - 14 && i >= this.maze.length - 17) && ((j == 12) || (j == 28))) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i == this.maze.length - 18) && ((j==12 || j==28) || (j>15 && j<25))) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i>=5 && i<=7) && (j==12 || j==20 || j==28)) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i==8) && (j<5 || j==8 || j==12 || (j>15 && j<25) || j==28 || j==32 || j>35)) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i>8 && i<12) && (j==8 || j==32)) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i==12) && ((j>3 && j<9) || (j>11 && j<17) || (j==20) || (j>23 && j<29) || (j>31 && j<37)) ) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i>12 && i<16) && j==20) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i==16) && ((j==12 || j==28) || (j>15 && j<25))) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i>16 && i<20) && (j==12 || j==28)) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i==20) && ((j==12 || j==28) || (j>15 && j<25))) {
+                    this.maze[i][j] = 1;
+                }
+    
+                if ( (i>20 && i<24) && (j==16 || j==24)) {
+                    this.maze[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    createBalls() {
+        for (i=2; i<this.maze.length; i+=2) {
+            for (j=2; j<this.maze[0].length; j+=2) {
+                if (this.maze[i][j] != 1) {
+                    this.maze[i][j] = 2;
+                }
+            }
+        }
+    }
+}
