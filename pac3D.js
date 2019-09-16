f68b36087bec5a912ad4e04c3e3fedf194cd9548
Edoardo Piroli <piroliedoardo@gmail.com>
2019-09-16 12:50:46 +0200

    Adds no change

--- pac3D.js
+++ pac3D.js
@@ -19,7 +19,7 @@ var player = {height: 10, speed: 0.15, turn_speed: Math.PI*0.015, wall_distance:
 var pacman;
 var pacman_x_dim = 1.5, pacman_y_dim = 1.5, pacman_z_dim = 1.5;
 
-//Maze
+// Maze
 var maze;
 
 var paused = true, settinged = false;
