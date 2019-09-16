bbaec653439ecb65b227d83a25f418f5a2eae34d
Luca Faraoni <faraoni.1667345@studenti.uniroma1.it>
2019-09-16 12:30:21 +0200

    bugged code to solve

--- pac3D.js
+++ pac3D.js
@@ -81,9 +81,8 @@ function init() {
         });
 
     var ghost = new Ghost();
-    ghost.init_ghost(1, 1, 1, 0xf000ff, 1, 0xf2008f, 1, 0xf0f01f, 0.2, 0.2, 0.2, 0xf2008f, new THREE.Vector3(100, player.height, -6));
+    ghost.constructor.init_ghost(ghost);
     scene.add(ghost.ghost);
-    console.log(ghost.ghost);
     //Create a raycaster instance
     raycaster = new THREE.Raycaster();
 
