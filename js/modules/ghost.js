1bbbaa1c450975571f29e1830f781f6137205370
Edoardo Piroli <piroliedoardo@gmail.com>
2019-09-17 19:28:29 +0200

    Changed ghost.js

--- js/modules/ghost.js
+++ js/modules/ghost.js
@@ -1,26 +1,26 @@
 export default class Ghost {
-    constructor() {
+    constructor(type) {
         this.ghost;
+        this.type = type;
     }
 
-    loadGhost(loader) {
+    loadGhost(loader, color) {
         loader.load(
             '3DModels/pacman_ghost.obj', 
     
 			(object) => {
-				object.scale.set(1.5, 1.5, 1.5);
-                object.position.set(110, 5, -10);
+                object.scale.set(4, 3, 4);
+                
+                // Standard spawn location
+                object.position.set(100, 3, -130);
 			
-				/* object.traverse( (child) => {
-					if (child instanceof THREE.Mesh ) {
-						//child.material.color.setHex(0xffff00);
+				object.traverse( (child) => {
+					if (child instanceof THREE.Mesh) {
+						child.material.color.setHex(color);
 					}
-                }); */
-
+                });
                 this.ghost = object;
             }
         );
-
-
     }
 }
